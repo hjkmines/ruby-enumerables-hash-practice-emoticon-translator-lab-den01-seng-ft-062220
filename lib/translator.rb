9d66d830a396a07010b2c85649764721a28a9966
Hyung Kim <hjkmines@gmail.com>
2020-06-18 18:33:52 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,6 +7,15 @@ def load_library(yaml_file)
 
 library = YAML.load_file(yaml_file)
 
+new_library = {}
+
+library.each do |meaning, emoticons|
+    new_library[meaning] = {english: emoticons[0], japanese: emoticons[1]}
+end 
+
+new_library
+binding.pry 
+puts ""
 end 
 
 
